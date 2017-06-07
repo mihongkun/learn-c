cc84554f1625711e40f6bc2e4db73102c82aafa3
mihongkun <mihongkun@gmail.com>
2017-06-07 23:05:27 +0800

    强制类型转换举例

--- main.c
+++ main.c
@@ -43,26 +43,35 @@ int main() { /*这里没有主函数名称的 -> 这里输入我们的主函数*/
     printf("年龄: %d 岁\n", age);
     printf("身高: %d 厘米\n", height);
     printf("体重: %f 千克\n", weight);
-    printf("是否属于肥胖儿童: %c\n",isFat);
+    printf("是否属于肥胖儿童: %c\n", isFat);
 
     int xAge = 18;
     float xHeight = 1.85;
     char xUnit = 'M';
-    printf("小明今年%d岁\n",xAge);
-    printf("小明的身高%.2f%c\n",xHeight,xUnit);
+    printf("小明今年%d岁\n", xAge);
+    printf("小明的身高%.2f%c\n", xHeight, xUnit);
     printf("小明现在在慕课网上学习IT技术\n");
 
-    printf("小明今天又得到了%d元零花钱\n",POCKETMONEY);
+    printf("小明今天又得到了%d元零花钱\n", POCKETMONEY);
 
+    /*
+     * 自动类型转换
+     */
     char char_c = 'a';
     int int_n = char_c;
     float float_f = int_n;
     double double_d = float_f;
-    printf("char_c = %c\n",char_c);
-    printf("int_n = %d\n",int_n);
-    printf("float_f = %.2f\n",float_f);
-    printf("double_d = %lf\n",double_d);
-    
+    printf("char_c = %c\n", char_c);
+    printf("int_n = %d\n", int_n);
+    printf("float_f = %.2f\n", float_f);
+    printf("double_d = %lf\n", double_d);
+
+
+    /*
+     * 强制类型转换 会影响精度
+     */
+    double double_num = 2.5;
+    printf("double_num的整数部分是%d\n", (int) double_num);
 
 
     return 0;
