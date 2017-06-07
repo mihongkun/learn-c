b835e5b0bb577614bef91c680a48c9edbf34bb6c
mihongkun <mihongkun@gmail.com>
2017-06-07 19:42:19 +0800

    继续修改

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
  */
 #include<stdio.h> /* 这里好像缺少头文件哦*/
 
-int main() { /*这里没有主函数名称的*/
+int main() { /*这里没有主函数名称的 -> 这里输入我们的主函数*/
     printf("Hello World!"); /*看下打印语句是否正确*/
+    printf("C程序中一定是从我开始的");
     return 0;
 }
 
