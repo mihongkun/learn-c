aa0b1a7adc807cf0e52de73fa3cd4198b59713fc
mihongkun <mihongkun@gmail.com>
2017-06-07 23:17:35 +0800

    运算符2

---
+++ 0002.operating.c
@@ -0,0 +1,25 @@
+//
+// Created by mihon on 2017/6/7.
+//
+
+#include <stdio.h>
+
+/**
+ * 运算符
+ * +
+ * -
+ * *
+ * /
+ * @return
+ */
+int main() {
+    int x, y;
+    x = 10;
+    y = -3;
+    printf("x + y = %d\n", x + y);
+    printf("x - y = %d\n", x - y);
+    printf("x * y = %d\n", x * y);
+    printf("x / y = %d\n", x / y);
+    printf("x %% y = %d\n", x % y);
+    return 0;
+}
