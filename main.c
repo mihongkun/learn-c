b74c504a892705f945b3ffb50f8c1bfb8802c499
mihongkun <mihongkun@gmail.com>
2017-06-07 19:36:00 +0800

    修改hello world
    添加注释说明

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 //
 // Created by mihon on 2017/6/7.
 //
-#include<stdio.h>
+/*
+ *  这里是多行注释
+ *  clion 构建快捷键 ctrl + f9
+ *        运行快借鉴 shift + f10
+ */
+#include<stdio.h> /* 这里好像缺少头文件哦*/
 
-int main() {
-    printf("Hello World!");
+int main() { /*这里没有主函数名称的*/
+    printf("Hello World!"); /*看下打印语句是否正确*/
     return 0;
 }
 
