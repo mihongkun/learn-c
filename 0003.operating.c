5f4753cc95901fc99085f9fc867c449298c7f239
mihongkun <mihongkun@gmail.com>
2017-06-07 23:26:05 +0800

    自增自减操作

---
+++ 0003.operating.c
@@ -0,0 +1,21 @@
+//
+// Created by mihon on 2017/6/7.
+//
+
+#include <stdio.h>
+
+/**
+ * 自增自减操作
+ * @return
+ */
+int main() {
+    int x = 0;
+    printf("x++ = %d\n", x++);
+    printf("++x = %d\n", ++x);
+    printf("x-- = %d\n", x--);
+    printf("--x = %d\n", --x);
+    printf("x -= 1 = %d\n", x -= 1);
+    printf("x += 1 = %d\n", x += 1);
+    return 0;
+}
+
